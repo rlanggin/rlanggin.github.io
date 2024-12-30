c901e4f226ef5505b45fb1200c1e235403339640
rlanggin <rachellanggin@gmail.com>
2024-12-30 21:25:15 +0000

    Deploying to gh-pages from @ rlanggin/rlanggin.github.io@0dd77fc5f65e31a4938ce7bcc18153e25c3c4039 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,28 +70,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/typograms/";
         
       },
-    },{id: "post-a-post-that-can-be-cited",
-      
-        title: "a post that can be cited",
-      
-      description: "this is what a post that can be cited looks like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/post-citation/";
-        
-      },
-    },{id: "post-a-post-with-pseudo-code",
-      
-        title: "a post with pseudo code",
-      
-      description: "this is what included pseudo code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/pseudocode/";
-        
-      },
     },{id: "news-ever-wondered-what-it-s-like-to-stargaze-at-mt-charleston",
           title: 'Ever wondered what it’s like to stargaze at Mt. Charleston?',
           description: "",
