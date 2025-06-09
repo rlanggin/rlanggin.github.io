2e8c32393ddfd85b87cb7d2b6ada499b83817704
rlanggin <rachellanggin@gmail.com>
2025-06-09 20:33:03 +0000

    Deploying to gh-pages from @ rlanggin/rlanggin.github.io@2474ed31a6972c23130841c378de7633ef0ed417 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Follow the Beginning Python Tutorials respository below to begin your coding for Physics and Astronomy adventure!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
